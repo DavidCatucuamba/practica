954525533b359418facb37d9f5c3527f11c28334
POO <ariel.catucuamba@epn.edu.ec>
2024-05-23 09:41:05 -0500

    La  hija Lupe hereda de la hija Maria

--- src/practico.java
+++ src/practico.java
@@ -11,11 +11,11 @@ public class practico {
     }
 
 }
-class edad extends practico{
+class Maria extends practico{
     //atributos
     int edad;
     double estatura;
-    public edad(String Nombre, String Apellido, int id,int edad,double estatura){
+    public Maria(String Nombre, String Apellido, int id,int edad,double estatura){
         super(Nombre,Apellido,id);
         this.edad=edad;
         this.estatura=estatura;
@@ -27,6 +27,32 @@ class edad extends practico{
         System.out.println("Id: "+id);
         System.out.println("Edad: "+ edad);
         System.out.println("Estatura: "+estatura);
+
+    }
+}
+//hija heredando a otra hija
+class Lupe extends Maria{
+    //Atributo
+    String tipo_pelo;
+    String cumpleaños;
+    String color_ojos;
+    public Lupe(String Nombre, String Apellido, int id,int edad,double estatura,String tipo_pelo,String cumpleaños,String color_ojos){
+        super(Nombre, Apellido, id, edad, estatura);
+        this.tipo_pelo=tipo_pelo;
+        this.cumpleaños=cumpleaños;
+        this.color_ojos=color_ojos;
+    }
+    public void Mostrar1(){
+        System.out.println("Datos");
+        System.out.println("Nombre: "+Nombre);
+        System.out.println("Apellido: "+Apellido);
+        System.out.println("Id: "+id);
+        System.out.println("Edad: "+ edad);
+        System.out.println("Estatura: "+estatura);
+        System.out.println("Pelo: "+tipo_pelo);
+        System.out.println("Color ojos: "+color_ojos);
+        System.out.println("Cumpleaños: "+cumpleaños);
+
 
     }
 }
