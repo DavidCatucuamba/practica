39810e3e028ef6d05618eca1bec1f09801367d15
POO <ariel.catucuamba@epn.edu.ec>
2024-05-23 09:09:21 -0500

    heredacion de la clase superior practico

--- src/practico.java
+++ src/practico.java
@@ -3,4 +3,27 @@ public class practico {
     String Nombre;
     String Apellido;
     int id;
+    //metodo constructor
+    public practico(String Nombre,String Apellido, int id){
+        this.Nombre=Nombre;
+        this.Apellido=Apellido;
+        this.id=id;
+    }
+
+}
+class edad extends practico{
+    //atributos
+    int edad;
+    public edad(String Nombre, String Apellido, int id,int edad){
+        super(Nombre,Apellido,id);
+        this.edad=edad;
+    }
+    public void Mostrar(){
+        System.out.println("Datos");
+        System.out.println("Nombre: ");
+        System.out.println("Apellido: ");
+        System.out.println("Id: ");
+        System.out.println("Edad: ");
+
+    }
 }
